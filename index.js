339cdd9642a0024f1ea625b838c9323521a8b76f
kamran-11003 <128039037+kamran-11003@users.noreply.github.com>
2025-01-09 23:13:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 10000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
